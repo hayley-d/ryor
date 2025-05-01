7ed21e3bb97031d9275351330fcbad2e8e0f8645
hayley-d <hayleydod@proton.me>
2025-05-01 03:41:54 +0200

    replace main with _start

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 #![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
-/// Function is called on panic.
+// Don't mangle the name of this function
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+    loop {}
+}
+
+/// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
-
-fn main() {}
