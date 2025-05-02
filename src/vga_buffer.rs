db5f5d421bb4ccb95440c919dee99a9a76841c24
hayley-d <hayleydod@proton.me>
2025-05-02 12:39:15 +0200

    fix cargo toml

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,5 +1,14 @@
 use core::fmt;
+use lazy_static::lazy_static;
 use volatile::Volatile;
+
+lazy_static! {
+    pub static ref WRITER: Writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+}
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
