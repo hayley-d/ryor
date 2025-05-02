e94e5351f863c2d61361caf3d6534289509b74bf
hayley-d <hayleydod@proton.me>
2025-05-02 12:42:59 +0200

    use macro in main

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,7 @@ static HELLO: &[u8] = b"Hello World!";
 // Don't mangle the name of this function
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(
-        vga_buffer::WRITER.lock(),
-        ", some numbers: {} {}",
-        42,
-        1.337
-    )
-    .unwrap();
+    println!("Hello World{}", "!");
     loop {}
 }
 
