6ca42602f1fa41015f3fbdbc38544390fb3918bc
hayley-d <hayleydod@proton.me>
2025-05-02 12:43:43 +0200

    update panic handler

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-/// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
