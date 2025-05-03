7d51b8874db70f744eca7fa070a07f38dd167039
hayley-d <hayleydod@proton.me>
2025-05-03 04:46:03 +0200

    add panic handler tests

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,19 @@ pub extern "C" fn _start() -> ! {
     exit_qemu(QemuExitCode::Success);
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
