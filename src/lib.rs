44ecd357aa4c60a487c25cd7f6f9f24d2ba11466
hayley-d <hayleydod@proton.me>
2025-05-03 04:54:19 +0200

    move over tests from main

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,8 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
-
+pub mod serial;
+pub mod vga_buffer;
 use core::panic::PanicInfo;
 
 pub trait Testable {
@@ -48,4 +49,20 @@ pub extern "C" fn _start() -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     test_panic_handler(info)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
