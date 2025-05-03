faa8d9bd6da1dff336aaeaf6be9db8f675dec21f
hayley-d <hayleydod@proton.me>
2025-05-03 04:59:10 +0200

    create tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -3,8 +3,20 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![test_runner(blog_os::test_runner)]
 
+use blog_os::println;
 use core::panic::PanicInfo;
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
+}
 
 #[unsafe(no_mangle)] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
