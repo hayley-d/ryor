17499d9ba4d5d1438472f5a81b750458daa8c7b5
hayley-d <hayleydod@proton.me>
2025-05-08 08:05:25 +0200

    update init function

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -4,6 +4,7 @@ use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 pub fn init_idt() {
     let mut idt = InterruptDescriptorTable::new();
     idt.breakpoint.set_handler_fn(breakpoint_handler);
+    idt.load();
 }
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
